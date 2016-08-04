25a247fd6254659d27d6fb3e00069f5e9587829a
CoolmanCZ <coolman@centrum.cz>
2016-08-04 13:15:09 +0200

    Simplification of the example.sh

--- GenerateCMakeFiles-lib.sh
+++ GenerateCMakeFiles-lib.sh
@@ -938,6 +938,32 @@ get_upp_to_process()
 
 }
 
+generate_package_file()
+{
+    echo -n "Creating archive "
+
+    declare -A sorted_UPP_ALL_USES_DONE=$(printf "%s\n" "${UPP_ALL_USES_DONE[@]}" | sort -u);
+
+    package_src_name_archive=$(basename ${PROJECT_NAME}).tar.bz2
+    package_src_name_archive_list="package_archive_list.txt"
+
+    echo "CMakeLists.txt" > ${package_src_name_archive_list}
+
+    find $(dirname ${PROJECT_NAME}) -name '*' -type f >> ${package_src_name_archive_list}
+
+    echo "${UPP_SRC_DIR}/uppconfig.h" >> ${package_src_name_archive_list}
+    echo "${UPP_SRC_DIR}/guiplatform.h" >> ${package_src_name_archive_list}
+
+    for pkg_name in ${sorted_UPP_ALL_USES_DONE[@]}; do
+        find ${UPP_SRC_DIR}/${pkg_name} -name '*' -type f >> ${package_src_name_archive_list}
+    done
+
+    tar -c -j -f ${package_src_name_archive} -T ${package_src_name_archive_list}
+    rm ${package_src_name_archive_list}
+
+    echo "... DONE"
+}
+
 generate_main_cmake_file()
 {
     local main_target="${1}"
@@ -1427,5 +1453,19 @@ target_link_libraries ( ${main_target_name}${BIN_SUFFIX} \${main_$LINK_LIST} ${l
 set_target_properties ( ${main_target_name}${BIN_SUFFIX} PROPERTIES OUTPUT_NAME ${main_target_name} )
 EOL
 # End of the cat (CMakeFiles.txt)
+
+    # Show used plugins
+    if [ "${GENERATE_DEBUG}" == "1" ]; then
+        declare -A sorted_UPP_ALL_USES=$(printf "%s\n" "${UPP_ALL_USES[@]}" | sort -u);
+        declare -A sorted_UPP_ALL_USES_DONE=$(printf "%s\n" "${UPP_ALL_USES_DONE[@]}" | sort -u);
+
+        echo "Plugins used   : " ${sorted_UPP_ALL_USES[@]}
+        echo "CMake generated: " ${sorted_UPP_ALL_USES_DONE[@]}
+    fi
+
+    # Generate package file
+    if [ "${GENERATE_PACKAGE}" == "1" ]; then
+        generate_package_file
+    fi
 }
 
--- example.sh
+++ example.sh
@@ -24,11 +24,11 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+source ./GenerateCMakeFiles-lib.sh
+
 GENERATE_VERBOSE="1"
 GENERATE_DEBUG="1"
 GENERATE_PACKAGE="1"
-
-source ./GenerateCMakeFiles-lib.sh
 
 UPP_SRC_BASE="upp-x11-src-2016-07-11"
 UPP_SRC_DIR="${UPP_SRC_BASE}/uppsrc"
@@ -40,37 +40,4 @@ PROJECT_FLAGS="-DflagGUI -DflagMT -DflagGCC -DflagLINUX -DflagPOSIX -DflagSHARED
 #PROJECT_FLAGS="-DflagLINUX -DflagPOSIX -DflagDEBUG"
 
 generate_main_cmake_file "${PROJECT_NAME}" "${PROJECT_FLAGS}"
-
-if [ "${GENERATE_DEBUG}" == "1" ]; then
-    declare -A sorted_UPP_ALL_USES=$(printf "%s\n" "${UPP_ALL_USES[@]}" | sort -u);
-    declare -A sorted_UPP_ALL_USES_DONE=$(printf "%s\n" "${UPP_ALL_USES_DONE[@]}" | sort -u);
-
-    echo "Plugins used   : " ${sorted_UPP_ALL_USES[@]}
-    echo "CMake generated: " ${sorted_UPP_ALL_USES_DONE[@]}
-fi
-
-if [ "${GENERATE_PACKAGE}" == "1" ]; then
-    echo -n "Creating archive "
-
-    declare -A sorted_UPP_ALL_USES_DONE=$(printf "%s\n" "${UPP_ALL_USES_DONE[@]}" | sort -u);
-
-    package_src_name_archive=$(basename ${PROJECT_NAME}).tar.bz2
-    package_src_name_archive_list="package_archive_list.txt"
-
-    echo "CMakeLists.txt" > ${package_src_name_archive_list}
-
-    find $(dirname ${PROJECT_NAME}) -name '*' -type f >> ${package_src_name_archive_list}
-
-    echo "${UPP_SRC_DIR}/uppconfig.h" >> ${package_src_name_archive_list}
-    echo "${UPP_SRC_DIR}/guiplatform.h" >> ${package_src_name_archive_list}
-
-    for pkg_name in ${sorted_UPP_ALL_USES_DONE[@]}; do
-        find ${UPP_SRC_DIR}/${pkg_name} -name '*' -type f >> ${package_src_name_archive_list}
-    done
-
-    tar -c -j -f ${package_src_name_archive} -T ${package_src_name_archive_list}
-    rm ${package_src_name_archive_list}
-
-    echo "... DONE"
-fi
 
