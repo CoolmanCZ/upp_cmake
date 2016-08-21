3b67e6582ab90d5b2fe4f09b6733c9919f78ec52
CoolmanCZ <coolman@centrum.cz>
2016-08-21 14:07:52 +0200

    Add build time measurement

--- utils/build.mingw32.sh
+++ utils/build.mingw32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+START=$(date +%s.%N)
 BUILD_DIR="build.mingw32"
 
 if [ -d $BUILD_DIR ]; then
@@ -9,5 +9,7 @@ fi
 mkdir -p $BUILD_DIR
 
 cd $BUILD_DIR
-cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32.cmake .. && make -j 4
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32.cmake .. && gmake -j 4
 
+DUR=$(echo "$(date +%s.%N) - ${START}" | bc)
+echo "Execution time: $(date -d@0${DUR} -u +%H:%M:%S.%N)"
